a1285fd14e933b8edfa97fc5a1a67c3b0bbec08c
Niall Thomson <nthomson@pivotal.io>
2020-03-03 07:33:28 -0800

    Var and output descriptions

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,31 +1,39 @@
 output "ops_manager_domain" {
-  value = module.pave.ops_manager_domain
+  description = "Domain for accessing OpsManager"
+  value       = module.pave.ops_manager_domain
 }
 
 output "ops_manager_username" {
-  value = module.pave.ops_manager_username
+  description = "Username for accessing OpsManager"
+  value       = module.pave.ops_manager_username
 }
 
 output "ops_manager_password" {
-  value = module.pave.ops_manager_password
+  description = "Password for accessing OpsManager"
+  value       = module.pave.ops_manager_password
 }
 
 output "cf_api_endpoint" {
-  value = module.common.api_endpoint
+  description = "Cloud Foundry API endpoint"
+  value       = module.common.api_endpoint
 }
 
 output "cf_apps_manager_endpoint" {
-  value = module.common.apps_manager_endpoint
+  description = "Cloud Foundry Apps Manager endpoint"
+  value       = module.common.apps_manager_endpoint
 }
 
 output "provisioner_host" {
-  value = module.pave.provisioner_host
+  description = "Hostname for accessing provisioner instance"
+  value       = module.pave.provisioner_host
 }
 
 output "provisioner_ssh_username" {
-  value = module.pave.provisioner_ssh_username
+  description = "SSH username for accessing provisioner instance"
+  value       = module.pave.provisioner_ssh_username
 }
 
 output "provisioner_ssh_private_key" {
-  value = module.pave.provisioner_ssh_private_key
+  description = "SSH password for accessing provisioner instance"
+  value       = module.pave.provisioner_ssh_private_key
 }
--- aws/vars.tf
+++ aws/vars.tf
@@ -1,42 +1,45 @@
 variable "env_name" {
-
+  description = "The name of the environment, used to name resources and DNS domains"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/20"
+  description = "The CIDR of the AWS VPC that will be created for this foundation"
+  default     = "10.0.0.0/20"
 }
 
 variable "region" {
-
+  description = "The AWS region in which to deploy the foundation (ie. us-west-2)"
 }
 
 variable "dns_suffix" {
-
+  description = "The suffix of the DNS domain that will be used (ie. aws.paasify.org)"
 }
 
 variable "availability_zones" {
-  description = "Optional list of availability zones, will be chosen automatically otherwise"
+  description = "List of AWS availability zones in which to deploy the foundation"
+  type        = list(string)
+}
+
+variable "pivnet_token" {
+  description = "Token for Pivotal Network used to download assets"
+}
+
+variable "ops_manager_instance_type" {
+  description = "AWS EC2 instance type used for OpsManager"
+  default     = "r4.large"
+}
+
+variable "pas_version" {
+  description = "The major version of PAS to install (ie. 2.8)"
+}
+
+variable "tiles" {
+  description = "List of names of tiles to install with PAS"
   type        = list(string)
   default     = []
 }
 
-variable "pivnet_token" {
-  
-}
-
-variable "ops_manager_instance_type" {
-  default = "r4.large"
-}
-
-variable "pas_version" {
-  description = "The major version of PAS to install"
-}
-
-variable "tiles" {
-  type = list
-  default = []
-}
-
 variable "encrypt_pas_buckets" {
-  default = "1"
+  description = "Apply KMS encryption to S3 buckets used for PAS"
+  default     = "1"
 }
