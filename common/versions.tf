7c51b9cfd3cf2f7a99c89bb17c4352aa696c45a7
Niall Thomson <nthomson@pivotal.io>
2020-03-20 16:33:49 -0700

    Pre-release support for PAS 2.9

--- common/versions.tf
+++ common/versions.tf
@@ -14,6 +14,11 @@ variable "ops_manager_versions" {
       version = "2.8.2"
       build   = "203"
     }
+
+    2.9 = {
+      version = "2.9.0"
+      build   = "106"
+    }
   }
 }
 
@@ -55,6 +60,23 @@ variable "tile_versions" {
 
     2.8 = {
       pas         = "2.8.1"
+      mysql       = "2.7.5"
+      redis       = "2.2.1"
+      rabbitmq    = "1.18.3"
+      scs         = "3.0.5"
+      metrics     = "1.6.1"
+      healthwatch = "1.8.0"
+      pcc         = "1.8.0"
+      credhub     = "1.3.2"
+      scdf        = "1.6.1"
+      antivirus   = "2.1.6"
+      gateway     = "1.0.2"
+      fim         = "2.0.0"
+      sso         = "1.11.0"
+    }
+
+    2.9 = {
+      pas         = "2.9.0-rc.2"
       mysql       = "2.7.5"
       redis       = "2.2.1"
       rabbitmq    = "1.18.3"
